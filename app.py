e47b2fe9580edfbb2d66acd0914f0221249aae70
ashishtiwari7307744088-afk <ashishtiwari7307744088@gmail.com>
2025-12-28 16:36:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,9 +23,12 @@ for i in X:
 scaler = StandardScaler()
 scaled_X = scaler.fit_transform(X)
 final_value = scaler.transform([all_value])
-
-model = st.cache(RandomForestRegressor)()
-model.fit(X,y)
+@st.cache_data
+def model_run():
+ model = RandomForestRegressor()
+ model.fit(X,y)
+ return model
+model = model_run(X,y)
 house_price = model.predict(final_value)[0]
 
 with st.spinner('predicting house price'):
@@ -37,6 +40,7 @@ st.success(msg)
 
 
 st.markdown('''**Design and Developed by:ashish kumar**''')
+
 
 
 
