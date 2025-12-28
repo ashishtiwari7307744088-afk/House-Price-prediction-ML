ef4d92560338860795b32aa0005ac0b35b93711c
ashishtiwari7307744088-afk <ashishtiwari7307744088@gmail.com>
2025-12-28 15:14:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,13 @@ from sklearn.datasets import fetch_california_housing
 
 st.title('house price prediction using ML')
 st.image('https://cdn.dribbble.com/userupload/23217976/file/original-203940eb89dec42ee25f6af662af24dc.gif')
+df = pd.read_csv('house_data.csv')
+X = df.iloc[:,:-3]
+y = df.iloc[:,-1]
+st.sidebar.title('Select house feature')
+st.sidebar.image('https://cdn.dribbble.com/userupload/23217976/file/original-203940eb89dec42ee25f6af662af24dc.gif')
+all_value = []
+for i in X:
+  ans = st.sidebar.slider(f'Select {i} value')
+  all_value.append(ans)
+st.write(all_value)
